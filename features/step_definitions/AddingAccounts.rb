e830cf0230f8682ad7b7b94f66121005cc7ee0bf
Luis Alvarez <lralvare@usc.edu>
2016-04-03 23:30:48 -0700

    updating tests

--- features/step_definitions/AddingAccounts.rb
+++ features/step_definitions/AddingAccounts.rb
@@ -1,63 +1,35 @@
-#require 'capybara/rspec'
-#require 'capybara/cucumber'
+#To get to the main page you HAVE to Login
 
-#include Capybara::DSL
-
-#Capybara.app_host = ""
-#Capybara.default_driver = :selenium
-
-#session = Capybara::Session.new :selenium
-#session.visit()
-
-#-------------------------------------------------------------------------------------------------
-
-Given /^I am on the main page and trying to add a new account$/ do
-    visit('http://localhost/Bancr/index.html')
-    #within('#logForm') do
-     #   fill_in 'email', :with => 'a@a.com'
-      #  fill_in 'password', :with => 'a'
-    #end
-    #click_button 'Login'
-end
-
-
-When(/^I try to add a new account without selecting a type or entering a name$/) do
-    find('#addAccount') do
-        fill_in 'accountName', :with => ''
-        fill_in 'accountType', :with => ''
-        click_button 'addAccount'
+Given /^I am on the main page trying to add a new account$/ do
+    visit('http://localhost/Bancr/index.php')
+    within('#logForm') do
+        fill_in 'email', :with => 'bancr@usc.edu'
+        fill_in 'password', :with => 'password'
     end
+    click_button 'Login'
 end
+#
+#
+#When(/^I try to add a new account with blank fields$/) do
+#    find('#addAccount') do
+#        fill_in 'accountName', :with => ''
+#        click_button 'addAccount'
+#    end
+#end
 
-Then(/^I should see an error message telling me to do so$/) do
-    expect(page).to have_content 'Please fill in the text fields'
-end
-
-
-
-When(/^I try to add a new account with only a type or only a name$/) do
-    find('#addAccount') do
-        fill_in 'accountName', :with => ''
-        fill_in 'accountType', :with => 'Checking'
-        click_button 'addAccount'
-    end
-end
-
-Then(/^I should see another message with appropriate instructions$/) do
-    expect(page).to have_content 'Please enter a name and select a type'
-
-end
-
-
-
-When(/^I try to add a new account with a proper name and type$/) do
+#Then(/^I should see an error message telling me to do so$/) do
+#    expect(page).to have_content 'Please enter a name for the account'
+#end
+#
+#
+#
+When(/^I try to add a new account with a proper name$/) do
     find('#addAccount') do
         fill_in 'accountName', :with => 'mynewaccount'
-        fill_in 'accountType', :with => 'Checking'
         click_button 'addAccount'
     end
 end
 
-Then(/^I should see a success message, and then see the account in my list\.$/) do
-    expect(page).to have_content 'Account value has updated'
+Then(/^I should see the account within a list$/) do
+    expect(page).to have_content 'mynewaccount'
 end
--- features/step_definitions/Login.rb
+++ features/step_definitions/Login.rb
@@ -1,11 +1,9 @@
-#-------------------------------------------------------------------------------------------------
-
 Given(/^I am on the login page$/) do
-    visit('http://localhost/Bancr/index.html')
+    visit('http://localhost/Bancr/index.php')
 end
 
 When(/^I try to login without credentials$/) do
-    within('#logForm') do
+    within('#form-signin') do
         fill_in 'email', :with => ''
         fill_in 'password', :with => ''
     end
@@ -13,13 +11,13 @@ When(/^I try to login without credentials$/) do
 end
 
 Then(/^I should see a login error message$/) do
-    expect(page).to have_content 'Please enter Username and Password'
+    expect(page).to have_content 'Please enter an email address and password'
 end
 
 
 
 When(/^I try to login with invalid credentials$/) do
-    within('#logForm') do
+    within('#form-signin') do
         fill_in 'email', :with => 'test@test.com'
         fill_in 'password', :with => 'b'
     end
@@ -33,21 +31,13 @@ end
 
 
 When(/^I try to login with valid credentials$/) do
-    within('#logForm') do
-        fill_in 'email', :with => 'a@a.com'
-        fill_in 'password', :with => 'a'
+    within('#form-signin') do
+        fill_in 'email', :with => 'bancr@usc.edu'
+        fill_in 'password', :with => 'password'
     end
     click_button 'Login'
 end
 
 Then(/^I should be successful to now see the main page$/) do
-    visit('http://localhost/Bancr/dashboard.html')
-end
-
-When(/^I click on the Registration Page Link$/) do
-    click_link("Register")
-end
-
-Then (/^I should see the Registration Page$/) do
-    expect(page).to have_content 'Register'
+    visit('http://localhost/Bancr/dashboard.php')
 end
