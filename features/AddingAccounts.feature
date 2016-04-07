71568599c9d53f9579fd731d4f12d35ae688ee6a
Kevin Shelton <shel746@usc.edu>
2016-04-06 20:57:20 -0800

    add account fix

--- features/AddingAccounts.feature
+++ features/AddingAccounts.feature
@@ -5,3 +5,6 @@ Scenario: User adds a new account
 Given I am on the main page and trying to add a new account2
 When I try to add a new account with a proper name and type
 Then I should see a success message, and then see the account in my list.
+
+When I try to add the same account
+Then I should get an error
--- features/step_definitions/AddingAccounts.rb
+++ features/step_definitions/AddingAccounts.rb
@@ -11,7 +11,7 @@
 
 #-------------------------------------------------------------------------------------------------
 
-Given /^I am on the main page and trying to add a new account2$/ do
+Given (/^I am on the main page and trying to add a new account2$/) do
     visit('http://localhost/Bancr/index.php')
     #within('#logForm') do
          fill_in 'email', :with => 'bancr@usc.edu'
@@ -27,4 +27,13 @@ end
 
 Then(/^I should see a success message, and then see the account in my list\.$/) do
     expect(page).to have_content 'mynewaccount'
+end
+
+When(/^I try to add the same account$/)do
+	fill_in 'accountName', :with => 'mynewaccount'
+	click_button 'addAccount'
+end
+
+Then (/^I should get an error$/)do
+	expect(page).to have_content 'Error: Account Name Already Exists'
 end
