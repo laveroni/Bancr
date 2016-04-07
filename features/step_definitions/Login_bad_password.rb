bd77633f207fd59e9b0ea14a21841845644d7e19
Kevin Shelton <shel746@usc.edu>
2016-04-06 19:13:55 -0800

    nvm about last push

--- features/step_definitions/Login_bad_password.rb
+++ features/step_definitions/Login_bad_password.rb
@@ -1,16 +1,14 @@
 Given(/^I am on the login page3$/) do
     visit('http://localhost/Bancr/index.php')
 end
-When(/^I try to login with valid credentials$/) do
+When(/^I try to login with invalid credentials$/) do
     #within('#logForm') do
         fill_in 'email', :with => 'bancr@usc.edu'
-        fill_in 'password', :with => 'password'
+        fill_in 'password', :with => 'a'
     #end
     click_button 'signInButton'
 end
 
-Then(/^I should be successful and now see the main page$/) do
-    page.should have_content('Accounts')
+Then(/^I should see an error message$/) do
+    page.should have_content('Enter your email and password')
 end
-
-
