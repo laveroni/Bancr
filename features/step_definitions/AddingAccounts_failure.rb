6cc254e555b11e6121ba33194accadf0d7846cc1
Kevin Shelton <shel746@usc.edu>
2016-04-06 20:48:15 -0800

    fixed

--- features/step_definitions/AddingAccounts_failure.rb
+++ features/step_definitions/AddingAccounts_failure.rb
@@ -27,5 +27,5 @@ When(/^I try to add a new account without selecting a type or entering a name$/)
 end
 
 Then(/^I should see an error message telling me to do so$/) do
-    expect(page).to have_content 'Bancr'
+    expect(page).to have_content 'Error: Enter Account Name'
 end
