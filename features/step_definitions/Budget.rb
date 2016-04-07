ad41aff15a9da3c322935d61c709e5692d6f59f5
Kevin Shelton <shel746@>
2016-04-06 19:56:07 -0700

    idk

--- features/step_definitions/Budget.rb
+++ features/step_definitions/Budget.rb
@@ -11,39 +11,3 @@
 
 #-------------------------------------------------------------------------------------------------
 
-Given /^I am on the main page and trying to add a new account$/ do
-   
-end
-
-
-When(/^I try to add a new account without selecting a type or entering a name$/) do
-    
-end
-
-Then(/^I should see an error message telling me to do so$/) do
-end
-
-
-
-When(/^I try to add a new account with only a type or only a name$/) do
-    
-end
-
-Then(/^I should see another message with appropriate instructions$/) do
-    expect(page).to have_content 'Please enter a name and select a type'
-
-end
-
-
-
-When(/^I try to add a new account with a proper name and type$/) do
-    find('#addAccount') do
-        fill_in 'accountName', :with => 'mynewaccount'
-        fill_in 'accountType', :with => 'Checking'
-        click_button 'addAccount'
-    end
-end
-
-Then(/^I should see a success message, and then see the account in my list\.$/) do
-    expect(page).to have_content 'Account value has updated'
-end
--- features/step_definitions/Logout.rb
+++ features/step_definitions/Logout.rb
@@ -3,7 +3,7 @@ Given(/^I am on the main application page$/) do
 end
 
 When(/^I try to logout$/) do
-    click_button 'Logout'
+    click_button('#logout')
 end
 
 Then(/^I should be successful to now see the login page$/) do
