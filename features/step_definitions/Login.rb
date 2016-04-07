e2702add5ea74d4a55e51deb504d76a4522950ca
Kevin Shelton <shel746@usc.edu>
2016-04-06 18:26:29 -0800

    fixes

--- features/step_definitions/Login.rb
+++ features/step_definitions/Login.rb
@@ -1,12 +1,12 @@
 Given(/^I am on the login page$/) do
-    
+    visit('http://localhost/Bancr/index.php')
 end
 
 When(/^I try to login without credentials$/) do
-    within('#logForm') do
+    #within('#logForm') do
         fill_in 'email', :with => ''
         fill_in 'password', :with => ''
-    end
+    #end
     click_button 'signInButton'
 end
 
@@ -15,14 +15,15 @@ Then(/^I should see a login error message$/) do
 end
 
 
+Given(/^I am on the login page2$/) do
+    visit('http://localhost/Bancr/index.php')
+end
 When(/^I try to login with invalid credentials$/) do
-    visit('http://localhost/Bancr/index.php')do
-        within('#logForm') do
-            fill_in 'email', :with => 'test@test.com'
-            fill_in 'password', :with => 'b'
-        end
-        click_button 'signInButton'
-    end
+   #within('#logForm') do
+        fill_in 'email', :with => 'test@test.com'
+        fill_in 'password', :with => 'b'
+    #end
+    click_button 'signInButton'
 end
 
 Then(/^I should see another error message$/) do
@@ -34,10 +35,10 @@ Given(/^I am on the login page3$/) do
     visit('http://localhost/Bancr/index.php')
 end
 When(/^I try to login with valid credentials$/) do
-    within('#logForm') do
+    #within('#logForm') do
         fill_in 'email', :with => 'bancr@usc.edu'
         fill_in 'password', :with => 'password'
-    end
+    #end
     click_button 'signInButton'
 end
 
