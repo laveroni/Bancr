2c20e89ce74685f7ff5689a2c5d528bd81168f3e
Kevin Shelton <shel746@usc.edu>
2016-04-09 22:38:28 -0800

    Db tests

--- scripts/Test/DbManagerTest.php
+++ scripts/Test/DbManagerTest.php
@@ -1,63 +1,20 @@
 <?php
 
-/*include_once "../account/account.php";
+include_once "../db/db_manager.php";
 include_once "../Transaction/transaction.php";
-//include_once "PHPUnit/Autoload.php";*/
-
-/*
-class AccountTest extends PHPUnit_Framework_TestCase{
+//include_once "PHPUnit/Autoload.php";
 
 
-	protected $account;
+class DbManagerTest extends PHPUnit_Framework_TestCase{
+
 	private $trans;
 
 	protected function setUp(){
 		$this->trans = array();
-		$this->account = new Account("Savings");
 	}
 
-	public function testSetNumber(){
-		$this->account->setNumber(50);
-		$this->assertEquals(50, $this->account->accountNumber);
+	public function testSetVars(){
+		$db = new dbManager();
 	}
-
-	public function testGetNumber(){
-		$this->account->number=50;
-		$expected = 50;
-		$actual = $this->getNumber();
-		$this->assertEquals($actual, $expected);
-	}
-
-	public function testGetName(){
-		$actual = $this->account->getName();
-		$expected = "Savings";
-		$this->assertEquals($expected, $actual);
-	}
-
-	public function testGetHistory(){
-		$actual = $this->account->getHistory();
-		$expected = $this->trans;
-		$this->assertEquals($actual, $expected);
-	}
-
-	public function testGetBalance(){
-		$expected = 0;
-		$actual = $this->account->getBalance();
-		$this->assertEquals($actual, $expected);
-	}
-
-	public function testGetLastTransaction(){
-		$tran = new Transaction("savings", 2/31/23, 59, "tom");
-		$this->account->addTransaction($tran);
-		$this->assertEquals($tran, $this->account->getLastTransaction());
-	}
-
-	public function testAddTransaction(){
-		$prior = count($this->account->transactionHistory);
-		$tran = new Transaction("savings", 2/31/23, 59, "tom");
-		$this->account->addTransaction($tran);
-		$now = count($this->account->transactionHistory);
-		$this->assertEquals($prior, $now + 1);
-	}
-}*/
+}
 ?>
