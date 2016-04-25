51e7aa1588b75d3e0f062fddce6138be89831c39
Kevin Shelton <shel746@usc.edu>
2016-04-25 10:40:39 -0700

    asdfas

--- features/step_definitions/AlphabeticalPostCsv.rb
+++ features/step_definitions/AlphabeticalPostCsv.rb
@@ -27,8 +27,8 @@ Then /^I should see the accounts ordered:$/ do |table|
 end
 
 Then (/^Remove Accounts$/) do
-  first('removeAccount').click_button
-  first('removeAccount').click_button
-  first('removeAccount').click_button
+  first('removeAccount').click
+  first('removeAccount').click
+  first('removeAccount').click
 end
 
